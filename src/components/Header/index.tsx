814554d90be6892aabf3a74a8641504ada92fa0f
Gabriel Soares <soaresgabriel33@gmail.com>
2022-07-10 17:38:01 -0300

    feat: criando e estilizando o header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import Logo from '../../assets/logo.svg'
+import { Container, Content } from './styles'
+
+export function Header() {
+    return(
+        <Container>
+            <Content>
+                <img src={Logo} alt="GlMoney"/>
+                <button>Nova Transação</button>
+           </Content>
+        </Container>
+    )
+} 
