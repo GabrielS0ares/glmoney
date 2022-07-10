8607768d35d9b5cd0ab1af3c0634c7d8db6403c0
Gabriel Soares <soaresgabriel33@gmail.com>
2022-07-10 16:12:53 -0300

    feat: demostracao de como usar o styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import styled from 'styled-components'
+
+const Title = styled.h1` //para criar um compontenva vamos usar a primeiro letra maiuscula, e dentro das grazes vamos usar o codigo css puro para estilização
+  color: #8257e6;
+  font-size: 64px;
+`
+
 function App() {
   return (
     <div className="App">
-      <h1>Olha ele ae</h1>
+      <Title>Olha ele ae</Title> 
     </div>
   );
 }
