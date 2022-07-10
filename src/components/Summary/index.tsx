2ddb32e94235f43caf53dd6884dc7d16508dbae2
Gabriel Soares <soaresgabriel33@gmail.com>
2022-07-10 18:14:16 -0300

    feat: criacao do estrutura summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,36 @@
+import Entradas from '../../assets/entradas.svg'
+import Saidas from '../../assets/saidas.svg'
+import Total from '../../assets/total.svg'
+
+import { Container } from "./styles";
+
+
+export function Summary(){
+    return(
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={Entradas} alt='entradas' />
+                </header>
+                <strong>R$ 1000,00</strong>
+            </div>
+
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={Saidas} alt='saidas' />
+                </header>
+                <strong> - R$ 300,00</strong>
+            </div>
+
+            <div className='highlight-background'>
+                <header>
+                    <p>Total</p>
+                    <img src={Total} alt='total' />
+                </header>
+                <strong>R$ 700,00</strong>
+            </div>
+        </Container>
+    )
+}
