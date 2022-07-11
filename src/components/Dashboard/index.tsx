913f8859dfec3ee8a0dad494aa08f7d79e741467
Gabriel Soares <soaresgabriel33@gmail.com>
2022-07-10 21:13:06 -0300

    feat: adicionando tabela no dashboard

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from "../Dashboard/styles"
 import { Summary } from "../Summary"
+import { TransactionTable } from "../transactionTable"
 
 export function Dashboard() {
     return(
         <Container>
             <Summary />
+            <TransactionTable />
         </Container>
     )
 }
